366510ec43d5b780b3d8286cf780f18ed2dee53b
BauwenDR <BauwenDR@gmail.com>
2026-03-03 19:40:27 +0100

    Fixed MSVC specific compiler error

--- Demo/RotateAroundParentComponent.cpp
+++ Demo/RotateAroundParentComponent.cpp
@@ -3,11 +3,13 @@
 #include "GameObject.hpp"
 #include "Time.hpp"
 
+#include <numbers>
+
 namespace demo
 {
     void RotateAroundParentComponent::Update()
     {
-        constexpr float pi2{2.0f * std::numbers::pi};
+        constexpr float pi2{2.0f * std::numbers::pi_v<float>};
 
         // Rotate and then make sure angle is between 0 and 2pi
         m_currentAngle += m_rotateSpeed * Time::get_delta_time();
