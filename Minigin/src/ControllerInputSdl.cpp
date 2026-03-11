3915ee592d97418539af434461d6ce34d591586e
BauwenDR <BauwenDR@gmail.com>
2026-03-12 00:40:43 +0100

    All buttons bound for SDL

--- Minigin/src/ControllerInputSdl.cpp
+++ Minigin/src/ControllerInputSdl.cpp
@@ -1,12 +1,31 @@
+#include <unordered_map>
+
 #include "ControllerInput.hpp"
 #include "InputManager.hpp"
 
 namespace dae::InputManager::Controller
 {
+    const std::pmr::unordered_map<SDL_GamepadButton, ControllerKey> m_buttonMap {
+        {SDL_GAMEPAD_BUTTON_DPAD_UP, ControllerKey::DpadUp},
+        {SDL_GAMEPAD_BUTTON_DPAD_DOWN, ControllerKey::DpadDown},
+        {SDL_GAMEPAD_BUTTON_DPAD_LEFT, ControllerKey::DpadLeft},
+        {SDL_GAMEPAD_BUTTON_DPAD_RIGHT, ControllerKey::DpadRight},
+        {SDL_GAMEPAD_BUTTON_SOUTH, ControllerKey::A},
+        {SDL_GAMEPAD_BUTTON_EAST, ControllerKey::B},
+        {SDL_GAMEPAD_BUTTON_WEST, ControllerKey::X},
+        {SDL_GAMEPAD_BUTTON_NORTH, ControllerKey::Y},
+        {SDL_GAMEPAD_BUTTON_LEFT_SHOULDER, ControllerKey::L1},
+        {SDL_GAMEPAD_BUTTON_RIGHT_SHOULDER, ControllerKey::R1},
+        {SDL_GAMEPAD_BUTTON_LEFT_STICK, ControllerKey::LeftStick},
+        {SDL_GAMEPAD_BUTTON_RIGHT_STICK, ControllerKey::RightStick},
+        {SDL_GAMEPAD_BUTTON_BACK , ControllerKey::Select},
+        {SDL_GAMEPAD_BUTTON_START, ControllerKey::Start}
+    };
+
     SDLControllerInput::SDLControllerInput()
     {
         int gamepadCount{};
-        auto gamepadIds = SDL_GetGamepads(&gamepadCount);
+        const auto gamepadIds = SDL_GetGamepads(&gamepadCount);
 
         if (gamepadCount > 0)
         {
@@ -20,24 +39,10 @@ namespace dae::InputManager::Controller
 
         uint16_t inputMask{};
 
-        if (SDL_GetGamepadButton(m_pGamepad, SDL_GAMEPAD_BUTTON_DPAD_UP))
-        {
-            inputMask |= static_cast<int16_t>(ControllerKey::DpadUp);
-        }
-
-        if (SDL_GetGamepadButton(m_pGamepad, SDL_GAMEPAD_BUTTON_DPAD_DOWN))
-        {
-            inputMask |= static_cast<int16_t>(ControllerKey::DpadDown);
-        }
-
-        if (SDL_GetGamepadButton(m_pGamepad, SDL_GAMEPAD_BUTTON_DPAD_LEFT))
-        {
-            inputMask |= static_cast<int16_t>(ControllerKey::DpadLeft);
-        }
-
-        if (SDL_GetGamepadButton(m_pGamepad, SDL_GAMEPAD_BUTTON_DPAD_RIGHT))
-        {
-            inputMask |= static_cast<int16_t>(ControllerKey::DpadRight);
+        for (const auto&[sdlButton, miniginButton] : m_buttonMap) {
+            if (SDL_GetGamepadButton(m_pGamepad, sdlButton)) {
+                inputMask |= static_cast<int16_t>(miniginButton);
+            }
         }
 
         const auto buttonChanges = inputMask ^ m_heldButtons;
