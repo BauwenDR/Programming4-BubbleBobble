de76b74a44c0ab36edddd9cc7472e0378d4643b9
BauwenDR <BauwenDR@gmail.com>
2026-04-12 16:14:58 +0200

    Enemy platform AI start

---
+++ Game/Component/PlatformAiMovement.cpp
@@ -0,0 +1,77 @@
+#include "PlatformAiMovement.hpp"
+
+#include "Time.hpp"
+#include "Component/ColliderComponent.hpp"
+#include "Component/PhysicsComponent.hpp"
+#include "Event/Sdbm.hpp"
+
+void game::PlatformAiMovement::Start()
+{
+    m_physics = GetGameObject().GetComponent<dae::PhysicsComponent>();
+}
+
+void game::PlatformAiMovement::Update()
+{
+    // Every 2 seconds choose a new action to undertake (moving left/ right or jumping)
+    // When we are in a potential jump zone, we can jump if closest player is above us
+    // When colliding with a jump collision zone, jump if closest player is not below us
+
+    m_decisionCooldown += Time::timeDelta();
+    if (m_currentAction == PlatformAiAction::None || m_decisionCooldown > DECISION_TIMEOUT)
+    {
+        m_decisionCooldown = std::fmod(m_decisionCooldown, DECISION_TIMEOUT);
+        TakeNextMovementDecision();
+    }
+
+    if (m_overlappingJumpZone)
+    {
+        
+    }
+
+    switch (m_currentAction)
+    {
+        case PlatformAiAction::WalkingLeft:
+            m_physics->MoveHorizontal(-1.0f);
+            break;
+        case PlatformAiAction::WalkingRight:
+            m_physics->MoveHorizontal(1.0f);
+            break;
+        case PlatformAiAction::Jumping:
+            m_physics->Jump();
+            break;
+        case PlatformAiAction::None:
+        default:
+            TakeNextMovementDecision();
+            m_decisionCooldown = 0.0f;
+            break;
+    }
+}
+
+void game::PlatformAiMovement::Notify(uint32_t event, dae::ObserverData const *data)
+{
+    if (event != dae::sdbm_hash("on_collision_enter") && event != dae::sdbm_hash("on_collision_exit")) return;
+
+    if (data == nullptr) return;
+    const auto colliderData{dynamic_cast<dae::ColliderData const *>(data)};
+    if (colliderData == nullptr) return;
+
+    if (event == dae::sdbm_hash("on_collision_enter"))
+    {
+
+    }
+
+    if (event == dae::sdbm_hash("on_collision_exit"))
+    {
+
+    }
+}
+
+game::PlatformAiMovement::PlatformAiMovement(dae::GameObject &owner)
+    : GameComponent(owner)
+{
+}
+
+void game::PlatformAiMovement::TakeNextMovementDecision()
+{
+    m_currentAction = PlatformAiAction::WalkingLeft;
+}
---
+++ Game/Component/PlatformAiMovement.hpp
@@ -0,0 +1,51 @@
+#ifndef MINIGIN_PLATFORMAIMOVEMENT_HPP
+#define MINIGIN_PLATFORMAIMOVEMENT_HPP
+
+#include <cstdint>
+
+#include "Component/GameComponent.hpp"
+#include "Event/IObserver.hpp"
+
+namespace dae
+{
+    class PhysicsComponent;
+}
+
+namespace game
+{
+    enum class PlatformAiAction : uint8_t
+    {
+        None,
+        WalkingLeft,
+        WalkingRight,
+        Jumping
+    };
+
+    class PlatformAiMovement final : public dae::GameComponent, public dae::IObserver
+    {
+    public:
+        void Start() override;
+        void Update() override;
+
+        void Notify(uint32_t event, dae::ObserverData const *data) override;
+
+        explicit PlatformAiMovement(dae::GameObject &owner);
+        ~PlatformAiMovement() override = default;
+
+    private:
+        constexpr static float DECISION_TIMEOUT{2.0f};
+
+        dae::PhysicsComponent *m_physics{};
+        PlatformAiAction m_currentAction{};
+
+        float m_decisionCooldown{};
+
+        bool m_overlappingJumpZone{false};
+        bool m_overlappingPotentialJumpZone{false};
+
+        void TakeNextMovementDecision();
+    };
+}
+
+
+#endif //MINIGIN_PLATFORMAIMOVEMENT_HPP
